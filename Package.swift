ba6d29c846e52bc48757cfc745653312af32de24
Danil <>
2020-07-27 15:37:31 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxOperators",
-            dependencies: []),
+            dependencies: ["RxSwift"]),
         .testTarget(
             name: "RxOperatorsTests",
             dependencies: ["RxOperators"]),
