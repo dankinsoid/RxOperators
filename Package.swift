481c7b9e4c1ff05d1246cf34f38f58e68ff31bff
Danil <voidilov@gmail.com>
2021-01-11 13:59:22 +0300

    1.0.26

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["RxOperators"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
         .package(url: "https://github.com/dankinsoid/VDKit.git", from: "1.0.12")
     ],
     targets: [
